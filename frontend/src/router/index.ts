c8cb43298c9f7529e1ee31b43d54822d3223e325
IceeAn <76437910+IceeAn@users.noreply.github.com>
2024-05-23 23:16:08 +0800

    clean the vue project

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/LoginView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -10,12 +10,10 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/booking',
+      name: 'booking',
+      component: () => import('../views/BookingView.vue'),
+      meta: { requiresAuth: true }
     }
   ]
 })

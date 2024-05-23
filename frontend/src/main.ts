c94f668b2b0429b35a18a587e17fd6c649d5570b
IceeAn <76437910+IceeAn@users.noreply.github.com>
2024-05-23 23:16:52 +0800

    import bootstrap-icons

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import "bootstrap-icons/font/bootstrap-icons.css"
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'

9b882569b22807d17a29640be881aabfff63d5a8
IceeAn <76437910+IceeAn@users.noreply.github.com>
2024-05-23 23:19:19 +0800

    add navbar into app.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import StickyNavbar from "@/components/StickyNavbar.vue";
 </script>
 
 <template>
-  <RouterView />
+  <header>
+    <StickyNavbar />
+  </header>
+  <main class="p-2 p-md-5">
+    <RouterView />
+  </main>
+
 </template>
 
 <style scoped>

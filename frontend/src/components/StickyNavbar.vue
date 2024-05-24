6ac6e35a2d0ef5e1b47d8a7480e5f1ac220b518a
IceeAn <76437910+IceeAn@users.noreply.github.com>
2024-05-24 22:04:57 +0800

    complete sticky navbar

--- frontend/src/components/StickyNavbar.vue
+++ frontend/src/components/StickyNavbar.vue
@@ -1,41 +1,45 @@
 <script setup lang="ts">
+import router from "@/router";
 
+function logout() {
+  console.log('logout')
+  router.replace({name: 'login'})
+}
 </script>
 <template>
-  <nav class="navbar navbar-expand-md sticky-top bg-body-tertiary">
+  <nav class="navbar navbar-expand-md sticky-top bg-theme">
     <div class="container-fluid">
       <RouterLink class="navbar-brand" :to="{name:'booking'}">实验室预约</RouterLink>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
               aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <RouterLink class="nav-link active" aria-current="page" :to="{name:'booking'}">预约</RouterLink>
+            <RouterLink class="btn btn-nav text-start px-2 mx-md-2 nav-link active border-0" aria-current="page" :to="{name:'booking'}">预约</RouterLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">帮助</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">更多</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown link
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+            <RouterLink class="btn btn-nav text-start px-2 mx-md-2 nav-link border-0" :to="{name:'help'}">帮助</RouterLink>
           </li>
         </ul>
+        <div class="d-flex">
+          <ul class="navbar-nav me-auto mb-2 mb-md-0">
+            <li class="nav-item">
+              <a v-if="$route.name!=='login'" class="btn btn-nav text-start px-2 mx-md-2 nav-link border-0" @click="logout"><span
+                  class="bi bi-door-open-fill"> 退出登录</span></a>
+              <a v-else class="btn btn-nav text-start px-2 mx-md-2 nav-link"><span
+                  class="bi bi-house-door-fill"> 登录</span></a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </nav>
 </template>
 
-<style scoped>
-
+<style scoped lang="sass">
+.btn-nav:hover,
+.btn-nav:active
+  background-color: #0001 !important
 </style>

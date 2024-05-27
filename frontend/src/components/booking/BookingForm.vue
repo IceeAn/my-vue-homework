8d151df9d4b69b5e5f69f48d8ecb2cd906448475
IceeAn <76437910+IceeAn@users.noreply.github.com>
2024-05-28 01:25:16 +0800

    create booking form component

---
+++ frontend/src/components/booking/BookingForm.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <p>题目：实验室xxx第x节预约</p>
+  <p>预约形式：预约本周本节｜选择多周本节｜按周次区间预约</p>
+  <p>选择周次：按钮格式按课程表来</p>
+  <p>周次时段允许全部预约｜单双周预约</p>
+  <p>是否允许调剂： 不允许｜整体调剂｜分节调剂</p>
+  <p>预约用途：课程｜实验｜考试｜其他</p>
+</template>
+
+<style scoped lang="sass">
+
+</style>

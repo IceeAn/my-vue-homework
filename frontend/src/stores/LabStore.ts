f23fc7866d4cc596ad202f425ff74fb9072fbaf0
IceeAn <76437910+IceeAn@users.noreply.github.com>
2024-05-28 01:23:23 +0800

    fix a spell issue

--- frontend/src/stores/LabStore.ts
+++ frontend/src/stores/LabStore.ts
@@ -2,7 +2,7 @@ import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useLabStore = defineStore('lab', () => {
-    const lab = ref('实验室1')
+    const lab = ref('实验室A')
 
     return { lab }
 })

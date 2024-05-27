d3cbc3f6d7339a20374b470a6555c8ebdd3724c2
IceeAn <76437910+IceeAn@users.noreply.github.com>
2024-05-27 16:44:38 +0800

    edit login router and add help router

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,21 +1,25 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/LoginView.vue'
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/booking',
-      name: 'booking',
-      component: () => import('../views/BookingView.vue'),
-      meta: { requiresAuth: true }
-    }
-  ]
+    history: createWebHashHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'login',
+            component: () => import('../views/LoginView.vue'),
+        },
+        {
+            path: '/booking',
+            name: 'booking',
+            component: () => import('../views/BookingView.vue'),
+            meta: { requiresAuth: true }
+        },
+        {
+            path: '/help',
+            name: 'help',
+            component: () => import('../views/HelpView.vue'),
+        }
+    ]
 })
 
 export default router

6bfb8d9253a5de1fce2b16294fc4e0769fac7e17
IceeAn <76437910+IceeAn@users.noreply.github.com>
2024-05-27 16:46:35 +0800

    use store in schedule table

--- frontend/src/components/booking/ScheduleTable.vue
+++ frontend/src/components/booking/ScheduleTable.vue
@@ -1,42 +1,41 @@
 <script setup lang="ts">
-import { computed } from "vue";
 import TableCell from "@/components/booking/TableCell.vue";
-import type { Lesson } from "@/types";
+import { useWeekStore } from "@/stores/weekStore";
+import { useScheduleStore } from "@/stores/scheduleStore";
+import { storeToRefs } from "pinia";
+import { computed, reactive, ref } from "vue";
 
-defineProps<{
-  week?: number
-}>()
+const weekStore = useWeekStore()
+const { week } = storeToRefs(weekStore)
 
-let fakeScheduleTable : (Lesson|null)[][] = [
-  [{teacher:"t1",comment: "课程"}, null, {teacher: "t2",comment:"考试"}, {teacher: "t2"}],
-  [{teacher:"t1",comment: "课程"}, null, {teacher: "t2",comment:"考试"}, {teacher: "t2"}],
-  [{teacher:"t1",comment: "课程"}, null, {teacher: "t2",comment:"考试"}, {teacher: "t2"}],
-  [{teacher:"t1",comment: "课程"}, null, {teacher: "t2",comment:"考试"}, {teacher: "t2"}],
-  [{teacher:"t1",comment: "课程"}, null, {teacher: "t2",comment:"考试"}, {teacher: "t2"}],
-  [{teacher:"t1",comment: "课程"}, null, {teacher: "t2",comment:"考试"}, {teacher: "t2"}],
-  [{teacher:"t1",comment: "课程"}, null, {teacher: "t2",comment:"考试"}, {teacher: "t2"}],
-]
+const scheduleStore = useScheduleStore()
+const { schedule } = scheduleStore
+
+// defineProps<{
+//   week?: number
+// }>()
+
+let fakeScheduleTable = computed(()=>(schedule[week.value-1]))
 
 </script>
 <template>
-  week:{{ week }}
-    <table class="table table-sm table-borderless align-middle">
-      <thead>
-      <tr>
-        <th scope="row">#</th>
-        <th class="text-center" scope="col" v-for="day in '一二三四五六日'" :key="day">周{{ day }}</th>
-      </tr>
-      </thead>
-      <tbody>
-      <tr v-for="i in 8" :key="i">
-        <th scope="row">{{ i }}</th>
-        <template v-if="i%2 == 1">
-          <TableCell v-for="j in 7" :key="j" :item="fakeScheduleTable[j-1][i/2|0] || undefined"/>
-        </template>
-        <td></td>
-      </tr>
-      </tbody>
-    </table>
+  <table class="table table-sm table-borderless align-middle">
+    <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th class="text-center" scope="col" v-for="day in '一二三四五六日'" :key="day">周{{ day }}</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="i in 8" :key="i">
+      <th scope="row">{{ i }}</th>
+      <template v-if="i%2 == 1">
+        <TableCell v-for="j in 7" :key="j" :item="fakeScheduleTable[j-1][i/2|0] || undefined"/>
+      </template>
+      <td></td>
+    </tr>
+    </tbody>
+  </table>
 </template>
 
 <style scoped lang="sass">

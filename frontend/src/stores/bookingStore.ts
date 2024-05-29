1d897f17263e139459c202a33334928d212d42d3
IceeAn <76437910+IceeAn@users.noreply.github.com>
2024-05-30 03:12:50 +0800

    fix a data display issue

--- frontend/src/stores/bookingStore.ts
+++ frontend/src/stores/bookingStore.ts
@@ -5,15 +5,15 @@ export const useBookingStore = defineStore('booking', () => {
     const bookingType = ref('预约本周本节')
     const defaultLab = ref('实验室A')
     const week: number[] = reactive([])
-    const period = reactive({ weekday: 0, startPeriod: 0 })
+    const period = reactive({ weekday: 1, startPeriod: 1 })
     const adjust = ref('不允许')
     const purpose = ref('课程')
 
     function $reset() {
         bookingType.value = "预约本周本节";
         week.splice(0);
-        period.weekday = 0;
-        period.startPeriod = 0;
+        period.weekday = 1;
+        period.startPeriod = 1;
         adjust.value = "不允许";
         purpose.value = "课程";
     }

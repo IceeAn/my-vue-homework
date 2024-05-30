95b5b0e96be97e15ef2b0551593dc32a2c8b8077
IceeAn <76437910+IceeAn@users.noreply.github.com>
2024-05-30 23:48:36 +0800

    clean code

--- frontend/src/stores/weekStore.ts
+++ frontend/src/stores/weekStore.ts
@@ -4,11 +4,5 @@ import { defineStore } from 'pinia'
 export const useWeekStore = defineStore('week', () => {
     const week = ref(1)
 
-    // const doubleCount = computed(() => count.value * 2)
-    //
-    // function increment() {
-    //     week.value++
-    // }
-
     return { week }
 })

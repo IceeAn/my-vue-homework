0e8296b6cdd0e6c818d1db0f349f5d43338e22c5
IceeAn <76437910+IceeAn@users.noreply.github.com>
2024-05-31 14:56:57 +0800

    fix an id conflict

--- frontend/src/components/booking/course-management/AddCourseModal.vue
+++ frontend/src/components/booking/course-management/AddCourseModal.vue
@@ -41,14 +41,14 @@ function confirm(){
         <div class="row row-cols-1 row-cols-lg-2">
           <div class="col">
             <div class="form-floating mb-3">
-              <input type="text" class="form-control" id="course-name" placeholder="课程名称" v-model="operatingCourse.name">
-              <label for="course-name">课程名称</label>
+              <input type="text" class="form-control" id="course-name-add" placeholder="课程名称" v-model="operatingCourse.name">
+              <label for="course-name-add">课程名称</label>
             </div>
           </div>
           <div class="col">
             <div class="form-floating mb-3">
-              <input type="number" class="form-control" id="course-period" placeholder="学时" v-model="operatingCourse.creditHour">
-              <label for="course-period">学时</label>
+              <input type="number" class="form-control" id="course-period-add" placeholder="学时" v-model="operatingCourse.creditHour">
+              <label for="course-period-add">学时</label>
             </div>
           </div>
         </div>
